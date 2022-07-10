54b60754cdd8dac7a54bb68615d1c32213747162
Infikei <hckhong@naver.com>
2022-07-10 22:12:02 +0900

    minor update (indent 4 -> 2)

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 function link_home() {
-    location.href="https://infikei.github.io";
+  location.href="https://infikei.github.io";
 }
 function area_profile() {
-    location.href="https://infikei.github.io#profile";
+  location.href="https://infikei.github.io#profile";
 }
 function area_channels() {
-    location.href="https://infikei.github.io#channels";
+  location.href="https://infikei.github.io#channels";
 }
 function area_pages() {
-    location.href="https://infikei.github.io#pages";
+  location.href="https://infikei.github.io#pages";
 }
 function area_contact() {
-    location.href="https://infikei.github.io#contact";
+  location.href="https://infikei.github.io#contact";
 }
 function link_talk() {
-    window.open("https://pf.kakao.com/_xaeEyT");
+  window.open("https://pf.kakao.com/_xaeEyT");
 }
 function link_instagram() {
-    window.open("https://instagram.com/infikei_photo", "_blank");
+  window.open("https://instagram.com/infikei_photo", "_blank");
 }
 
 const toggleBtn = document.querySelector('.navbar_togglebtn');
@@ -25,6 +25,6 @@ const menu = document.querySelector('.navbar_menu');
 const sns = document.querySelector('.navbar_sns');
 
 toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active')
-    sns.classList.toggle('active')
+  menu.classList.toggle('active')
+  sns.classList.toggle('active')
 });
